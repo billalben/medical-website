f486e0c58c28737ec1c4985cefb370fb272e59b0
Benzazoua <billalben252@gmail.com>
2024-04-23 16:10:05 +0100

    Doctors component

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -6,30 +6,30 @@ import { FaHeartbeat } from "react-icons/fa";
 
 function Services() {
   const icon1 = (
-    <RiMicroscopeLine size={35} className=" text-backgroundColor" />
+    <RiMicroscopeLine size={35} className="text-backgroundColor" />
   );
   const icon2 = (
-    <MdHealthAndSafety size={35} className=" text-backgroundColor" />
+    <MdHealthAndSafety size={35} className="text-backgroundColor" />
   );
-  const icon3 = <FaHeartbeat size={35} className=" text-backgroundColor" />;
+  const icon3 = <FaHeartbeat size={35} className="text-backgroundColor" />;
 
   return (
-    <div className=" min-h-screen flex flex-col justify-center lg:px-32 px-5 pt-24 lg:pt-16">
-      <div className=" flex flex-col items-center lg:flex-row justify-between">
+    <div className="bg-[#ececec] min-h-screen flex flex-col justify-center lg:px-32 px-5 pt-16 lg:pt-5">
+      <div className="flex flex-col items-center lg:flex-row justify-between">
         <div>
-          <h1 className=" text-4xl font-semibold text-center lg:text-start">
+          <h1 className="text-4xl font-semibold text-center lg:text-start">
             Our Services
           </h1>
-          <p className=" mt-2 text-center lg:text-start">
+          <p className="mt-2 text-center lg:text-start">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus,
             quidem.
           </p>
         </div>
-        <div className=" mt-4 lg:mt-0">
+        <div className="mt-4 lg:mt-0">
           <Button title="See Services" />
         </div>
       </div>
-      <div className=" flex flex-col lg:flex-row gap-5 pt-14">
+      <div className="flex flex-col lg:flex-row gap-5 pt-14">
         <ServicesCard icon={icon1} title="Lab Test" />
         <ServicesCard icon={icon2} title="Health Check" />
         <ServicesCard icon={icon3} title="Heart Health" />
